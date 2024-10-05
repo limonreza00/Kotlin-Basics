5f036b0c0e6bbab52f5656e625bd9caec459d1da
limon <limonreza3.1416@gmail.com>
2024-10-05 23:31:00 +0600

    Functional Class and String Interpolation

--- app/src/main/java/com/coderscastle/kotlinbasics/HelloWorld.kt
+++ app/src/main/java/com/coderscastle/kotlinbasics/HelloWorld.kt
@@ -4,10 +4,21 @@ package com.coderscastle.kotlinbasics
 
 
 fun main (args : Array<String>){
-    myDetails()
+
+    val personObj = Person() //Object creation of Person class
+
+    println("Hy , this is ${personObj.name}") //Interpolation of String
+
 }
 
 
 fun myDetails (){
     print("My name is Android")
+}
+
+
+
+class Person (){
+
+    val name : String = "Android"
 }
